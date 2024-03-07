5525d55e507e9ffa83aff4157fa7929da8e5602e
meister42 <hgh585@utulsa.edu>
2024-03-07 16:27:08 -0600

    problem 2, test cases are wrong

--- Problem 2/wordInRange.py
+++ Problem 2/wordInRange.py
@@ -1,5 +1,19 @@
 def wordInRange():
-    #Type your code here
-    return
+    filename = input()
+    first_char = input()
+    second_char = input()
+    wordlist = open(filename, 'r').readlines()
+    wordlist2 = []
+    for i in range(0, len(wordlist)):
+        newterm = wordlist[i].strip()
+        wordlist2.append(newterm)
+    for i in range(0, len(wordlist2)):
+        if wordlist2.index(f"{first_char}") <= i <= wordlist2.index(f"{second_char}"):
+            print(f"{wordlist2[i]} - in range")
+        else:
+            print(f"{wordlist2[i]} - not in range")
+    
+
 if __name__ == '__main__':
     wordInRange()
+    
