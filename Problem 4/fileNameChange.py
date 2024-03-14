d7cd1d7e2de195c5deb8372c5a3860d0d5dff734
meister42 <hgh585@utulsa.edu>
2024-03-13 23:38:36 -0500

    idk what is wrong

--- Problem 4/fileNameChange.py
+++ Problem 4/fileNameChange.py
@@ -1,5 +1,21 @@
+"""
+Name: Henry Holman
+Lab Time: Thursday 2 pm
+"""
+
+
 def fileNameChange():
-    #type your code here
+    #get input
+
+    program = input("input program name\n")
+    with open(program, "r") as photolist:
+        name_file = photolist.readlines()
+        for line in range(0, len(name_file)):
+            image_name = name_file[line].split("_")[0]
+            new_file_name = f"{image_name}_info.txt"
+            print(new_file_name)
+            
+
 
     return
 
